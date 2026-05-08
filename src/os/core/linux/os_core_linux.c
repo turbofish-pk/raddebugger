81da71544ca82c83b29a9ec10fb9cb582079802e
Nikita Smith <git.nikita.smith@gmail.com>
2026-05-07 17:05:33 -0700

    add file reserve Linux OS API for radlink

--- src/os/core/linux/os_core_linux.c
+++ src/os/core/linux/os_core_linux.c
@@ -395,6 +395,13 @@ os_id_from_file(OS_Handle file)
 }
 
 internal B32
+os_file_reserve_size(OS_Handle file, U64 size)
+{
+  int status = fallocate((int)file.u64[0], FALLOC_FL_KEEP_SIZE, 0, size);
+  return status == 0;
+}
+
+internal B32
 os_delete_file_at_path(String8 path)
 {
   Temp scratch = scratch_begin(0, 0);
